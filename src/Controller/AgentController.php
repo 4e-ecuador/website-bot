192b980d309ba5242c06712e5f42408335fcf5cb
Nikolai Plath <der.el.kuku@gmail.com>
2019-06-20 17:32:03 -0500

    Restrict agent controller to admin

--- src/Controller/AgentController.php
+++ src/Controller/AgentController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Agent;
 use App\Form\AgentType;
 use App\Repository\AgentRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/agent")
+ * @IsGranted("ROLE_ADMIN")
  */
 class AgentController extends AbstractController
 {

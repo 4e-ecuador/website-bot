6595e13a65d5b3b049f5ca8b715328a5db665691
Nikolai Plath <der.el.kuku@gmail.com>
2019-09-18 10:35:48 -0500

    Add stats crud

---
+++ src/Controller/AgentStatController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\AgentStat;
+use App\Form\AgentStatType;
+use App\Repository\AgentStatRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/agent-stat")
+ */
+class AgentStatController extends AbstractController
+{
+    /**
+     * @Route("/", name="agent_stat_index", methods={"GET"})
+     * @IsGranted("ROLE_AGENT")
+     */
+    public function index(AgentStatRepository $agentStatRepository): Response
+    {
+        return $this->render('agent_stat/index.html.twig', [
+            'agent_stats' => $agentStatRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="agent_stat_new", methods={"GET","POST"})
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function new(Request $request): Response
+    {
+        $agentStat = new AgentStat();
+        $form = $this->createForm(AgentStatType::class, $agentStat);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($agentStat);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('agent_stat_index');
+        }
+
+        return $this->render('agent_stat/new.html.twig', [
+            'agent_stat' => $agentStat,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="agent_stat_show", methods={"GET"})
+     * @IsGranted("ROLE_AGENT")
+     */
+    public function show(AgentStat $agentStat): Response
+    {
+        return $this->render('agent_stat/show.html.twig', [
+            'agent_stat' => $agentStat,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="agent_stat_edit", methods={"GET","POST"})
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function edit(Request $request, AgentStat $agentStat): Response
+    {
+        $form = $this->createForm(AgentStatType::class, $agentStat);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('agent_stat_index');
+        }
+
+        return $this->render('agent_stat/edit.html.twig', [
+            'agent_stat' => $agentStat,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="agent_stat_delete", methods={"DELETE"})
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function delete(Request $request, AgentStat $agentStat): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$agentStat->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($agentStat);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('agent_stat_index');
+    }
+}
---
+++ src/Form/AgentStatType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\AgentStat;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AgentStatType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('datetime')
+            ->add('ap')
+            ->add('explorer')
+            ->add('recon')
+            ->add('seer')
+            ->add('trekker')
+            ->add('builder')
+            ->add('connector')
+            ->add('mindController')
+            ->add('illuminator')
+            ->add('recharger')
+            ->add('liberator')
+            ->add('pioneer')
+            ->add('engineer')
+            ->add('purifier')
+            ->add('specops')
+            ->add('hacker')
+            ->add('translator')
+            ->add('sojourner')
+            ->add('recruiter')
+            ->add('missionday')
+            ->add('nl1331Meetups')
+            ->add('ifs')
+            ->add('agent')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => AgentStat::class,
+        ]);
+    }
+}

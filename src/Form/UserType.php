c0fe59eb92972b1445d581cf4e512c59a315007b
Nikolai Plath <der.el.kuku@gmail.com>
2019-10-05 10:22:10 -0500

    Add tabs to import stats

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,7 +18,7 @@ class UserType extends AbstractType
         $builder
             ->add('username')
             ->add('email')
-            //            ->add('password', PasswordType::class)
+            ->add('password', PasswordType::class)
             ->add(
                 'roles',
                 ChoiceType::class,

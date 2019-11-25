4e24deb64d17fae7d457b9257e008bcd56cadcc2
Nikolai Plath <der.el.kuku@gmail.com>
2019-11-25 10:35:19 -0500

    Add support for multiple event countdown timers

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,11 +1,12 @@
 const $ = require('jquery')
 
 function getTimeRemaining(endtime){
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor( (t/1000) % 60 );
-    var minutes = Math.floor( (t/1000/60) % 60 );
-    var hours = Math.floor( (t/(1000*60*60)) % 24 );
-    var days = Math.floor( t/(1000*60*60*24) );
+    let t = Date.parse(endtime) - Date.parse(new Date())
+    let seconds = Math.floor((t / 1000) % 60)
+    let minutes = Math.floor((t / 1000 / 60) % 60)
+    let hours = Math.floor((t / (1000 * 60 * 60)) % 24)
+    let days = Math.floor(t / (1000 * 60 * 60 * 24))
+
     return {
         'total': t,
         'days': days,
@@ -15,49 +16,18 @@ function getTimeRemaining(endtime){
     };
 }
 
-function updateClock(){
-    let t = getTimeRemaining(deadline);
-    if(t.total<=0){
-        clearInterval(timeinterval);
-    }
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = t.hours;
-    minutesSpan.innerHTML = t.minutes;
-    secondsSpan.innerHTML = t.seconds;
-    // clock.innerHTML = 'days: ' + t.days + '<br>' +
-    //     'hours: '+ t.hours + '<br>' +
-    //     'minutes: ' + t.minutes + '<br>' +
-    //     'seconds: ' + t.seconds;
+function updateClock(clock){
+    let t = getTimeRemaining(clock.data('deadline'));
+    // if(t.total<=0){
+    //     clearInterval(timeinterval);
+    // }
+    clock.find('.days').html(t.days);
+    clock.find('.hours').html(t.hours);
+    clock.find('.minutes').html(t.minutes);
+    clock.find('.seconds').html(t.seconds);
 }
 
-
-function initializeClock(){
-    // var clock = document.getElementById(id);
-    updateClock(); // run function once at first to avoid delay
-    var timeinterval = setInterval(updateClock,1000);
-
-
-    // var timeinterval = setInterval(function(){
-    //     var t = getTimeRemaining(endtime);
-    //     clock.innerHTML = 'days: ' + t.days + ' '
-    //         + 'hours: '+ t.hours + ' '
-    //         + 'minutes: ' + t.minutes + ' '
-    //         + 'seconds: ' + t.seconds;
-    //     if(t.total<=0){
-    //         clearInterval(timeinterval);
-    //     }
-    // },1000);
-}
-
-let deadline = $('#js-data').data('deadline')
-
-if (deadline) {
-
-    var clock = document.getElementById('clockdiv');
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
-
-    initializeClock();
-}
+$('.clock-counter').each(function( index ) {
+    updateClock($(this))
+    setInterval(updateClock,1000, $(this))
+});

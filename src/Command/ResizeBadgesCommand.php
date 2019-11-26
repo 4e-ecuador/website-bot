06369d49a1053ffe6117b7d3dc1188615d397169
Nikolai Plath <der.el.kuku@gmail.com>
2019-11-26 16:13:00 -0500

    Add resizeBadges command

---
+++ src/Command/ResizeBadgesCommand.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ResizeBadgesCommand extends Command
+{
+    protected static $defaultName = 'ResizeBadges';
+
+    /**
+     * ScrapeBadgesCommand constructor.
+     *
+     * @param string $rootDir
+     */
+
+    public function __construct(string $rootDir)
+    {
+        $this->rootDir = $rootDir.'/assets/images/badges';
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $sizes = [50];
+
+        foreach ($sizes as $size) {
+            foreach (new \DirectoryIterator($this->rootDir) as $item) {
+                if ($item->isDot()) {
+                    continue;
+                }
+                $io->writeln($item->getRealPath());
+                $srcPath = $item->getRealPath();
+                if (strpos($srcPath, '_'.$size.'.png')) {
+                    continue;
+                }
+                $destPath = str_replace('.png', '_'.$size.'.png', $srcPath);
+                $command = 'convert '.$srcPath.' -resize '.$size.'x'.$size.'\> '
+                    .$destPath;
+                ob_start();
+                system($command, $return_var);
+                $output = ob_get_contents();
+                ob_end_clean();
+
+                if ($output) {
+                    $io->error($output);
+                }
+            }
+        }
+
+        $io->success('Finished!');
+
+        return 0;
+    }
+}

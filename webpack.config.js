70fa608e393887c81b655b6e89d50fcb93fec8c2
Nikolai Plath <der.el.kuku@gmail.com>
2019-12-28 19:23:35 -0500

    composer recipes:install symfony/webpack-encore-bundle --force -v

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-var Encore = require('@symfony/webpack-encore')
+var Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
 Encore
@@ -37,9 +37,6 @@ Encore
 
     .addEntry('paginator', './assets/js/paginator.js')
 
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
-
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
@@ -61,10 +58,9 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     // enables @babel/preset-env polyfills
-    .configureBabel(() => {
-    }, {
-        useBuiltIns: 'usage',
-        corejs: 3
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
     })
 
     // enables Sass/SCSS support
@@ -96,6 +92,6 @@ Encore
         // only copy files matching this pattern
         //pattern: /\.(png|jpg|jpeg)$/
     })
+;
 
-
-module.exports = Encore.getWebpackConfig()
+module.exports = Encore.getWebpackConfig();

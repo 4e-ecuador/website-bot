6e96766265758956384a27a8a823e352d2ad688a
Nikolai Plath <der.el.kuku@gmail.com>
2020-04-12 09:23:50 -0500

    Fix Access control

--- src/Controller/TestStatController.php
+++ src/Controller/TestStatController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\TestStat;
 use App\Form\TestStatType;
 use App\Repository\TestStatRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/test/stat")
+ * @IsGranted("ROLE_ADMIN")
  */
 class TestStatController extends AbstractController
 {

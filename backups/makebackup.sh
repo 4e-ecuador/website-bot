f869280a2bdad5ec6065fe6d445e0d4f19705993
elkuku <der.el.kuku@gmail.com>
2020-05-09 11:37:27 -0500

    Adjust backup script

--- backups/makebackup.sh
+++ backups/makebackup.sh
@@ -7,8 +7,10 @@ echo Starting Backup...
 
 rm -f latest.dump
 
-heroku pg:backups:capture --remote heroku-prod
-heroku pg:backups:download --remote heroku-prod
+heroku pg:backups:capture --app agents-4e
+heroku pg:backups:download --app agents-4e
+#heroku pg:backups:capture --remote heroku-prod
+#heroku pg:backups:download --remote heroku-prod
 
 pg_restore latest.dump > "$current_time""_dump.sql"
 

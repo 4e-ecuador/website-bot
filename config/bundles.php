8be0d5da73ae95948fe6b3901f90ba4fb175ec55
elkuku <der.el.kuku@gmail.com>
2020-06-16 12:20:57 -0500

    Add API

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,6 @@ return [
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];

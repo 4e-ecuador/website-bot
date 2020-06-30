8aea54dfcbbcda27e7685f0e1c0e9df297dafdc5
elkuku <der.el.kuku@gmail.com>
2020-06-30 14:52:14 -0500

    Add a total counter to ingress events overview page

--- assets/js/ingress-events/overview.js
+++ assets/js/ingress-events/overview.js
@@ -14,9 +14,27 @@ for (let i in eventIds) {
             const tmpl = $.templates('#myTemplate')
             const html = tmpl.render(result)
             out.html(html)
+            updateTotal()
         }, error: function(xhr){
             out.html("An error occured: " + xhr.status + " " + xhr.statusText)
         }
     })
+}
+
+function updateTotal() {
+    const outEnl = $('#total-enl')
+    const outRes = $('#total-res')
+    let totalEnl = 0
+    let totalRes = 0
+    $('.counter').each(function(){
+        if ($(this).hasClass('faction-enl')) {
+            totalEnl += parseInt($(this).html())
+        } else {
+            totalRes += parseInt($(this).html())
+        }
+    })
+    outEnl.html(totalEnl)
+    outRes.html(totalRes)
+    $('#total').html(parseInt(outEnl.html()) + parseInt(outRes.html()))
 }
 

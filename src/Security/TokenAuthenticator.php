f4fa5d6345f6709d3143c55d6e1cffaca8d4e083
elkuku <der.el.kuku@gmail.com>
2020-06-30 11:03:53 -0500

    Add token authentication

---
+++ src/Security/TokenAuthenticator.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class TokenAuthenticator extends AbstractGuardAuthenticator
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function supports(Request $request): bool
+    {
+        $token = $request->headers->get('X-AUTH-TOKEN');
+
+        return $token ? true : false;
+        // return $request->headers->has('X-AUTH-TOKEN');
+    }
+
+    public function getCredentials(Request $request)
+    {
+        $token = $request->headers->get('X-AUTH-TOKEN');
+
+        $scheme = $request->getScheme();
+
+        if ('https' !== $scheme) {
+            throw new CustomUserMessageAuthenticationException(
+                'Please use SSL and not: '.$scheme
+            );
+        }
+
+        if ($token === 'ILuvAPIs') {
+            throw new CustomUserMessageAuthenticationException(
+                'ILuvAPIs is not a real API key: it\'s just a silly phrase'
+            );
+        }
+
+        return $token;
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        if (null === $credentials) {
+            // The token header was empty, authentication fails with HTTP Status
+            // Code 401 "Unauthorized"
+            return null;
+        }
+
+        // if a User is returned, checkCredentials() is called
+        return $this->em->getRepository(User::class)
+            ->findOneBy(['apiToken' => $credentials]);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return true;
+    }
+
+    public function onAuthenticationSuccess(
+        Request $request,
+        TokenInterface $token,
+        $providerKey
+    ) {
+        return null;
+    }
+
+    public function onAuthenticationFailure(
+        Request $request,
+        AuthenticationException $exception
+    ) {
+        $data = [
+            'message' => strtr(
+                $exception->getMessageKey(),
+                $exception->getMessageData()
+            )
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function start(
+        Request $request,
+        AuthenticationException $authException = null
+    ) {
+        $data = [
+            'message' => 'Authentication Required',
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function supportsRememberMe()
+    {
+        return false;
+    }
+}

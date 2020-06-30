97a1dc348d942719b66aadebf6285bad2fa83a1e
elkuku <der.el.kuku@gmail.com>
2020-06-30 11:04:11 -0500

    [DBMIGRATION]]

---
+++ src/Migrations/Version20200629153220.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200629153220 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE agent_user ADD api_token VARCHAR(255) DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_20086CAA7BA2F5EB ON agent_user (api_token)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP INDEX UNIQ_20086CAA7BA2F5EB');
+        $this->addSql('ALTER TABLE agent_user DROP api_token');
+    }
+}

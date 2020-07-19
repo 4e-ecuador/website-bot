f6801b9f2ab892b7b9c13e25df256385963028e1
elkuku <der.el.kuku@gmail.com>
2020-07-19 14:09:29 -0500

    Update recipes

--- src/Kernel.php
+++ src/Kernel.php
@@ -11,20 +11,15 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
-    protected function configureContainer(ContainerConfigurator $container
-    ): void {
+    protected function configureContainer(ContainerConfigurator $container): void
+    {
         $container->import('../config/{packages}/*.yaml');
-        $container->import(
-            '../config/{packages}/'.$this->environment.'/*.yaml'
-        );
+        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
 
-        if (file_exists(\dirname(__DIR__).'/config/services.yaml')) {
+        if (is_file(\dirname(__DIR__).'/config/services.yaml')) {
             $container->import('../config/{services}.yaml');
-            $container->import(
-                '../config/{services}_'.$this->environment.'.yaml'
-            );
-        } else {
-            $path = \dirname(__DIR__).'/config/services.php';
+            $container->import('../config/{services}_'.$this->environment.'.yaml');
+        } elseif (is_file($path = \dirname(__DIR__).'/config/services.php')) {
             (require $path)($container->withPath($path), $this);
         }
     }
@@ -34,10 +29,9 @@ class Kernel extends BaseKernel
         $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
         $routes->import('../config/{routes}/*.yaml');
 
-        if (file_exists(\dirname(__DIR__).'/config/routes.yaml')) {
+        if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
             $routes->import('../config/{routes}.yaml');
-        } else {
-            $path = \dirname(__DIR__).'/config/routes.php';
+        } elseif (is_file($path = \dirname(__DIR__).'/config/routes.php')) {
             (require $path)($routes->withPath($path), $this);
         }
     }

c18f6aae358927558053ac28647bbf5c1e2966c3
elkuku <der.el.kuku@gmail.com>
2020-07-31 11:20:52 -0500

    Update badge scraper

--- src/Command/UpdateBadgeDataCommand.php
+++ src/Command/UpdateBadgeDataCommand.php
@@ -8,9 +8,7 @@ use DOMXPath;
 use RuntimeException;
 use stdClass;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use function Symfony\Component\String\u;
@@ -38,19 +36,7 @@ class UpdateBadgeDataCommand extends Command
 
     protected function configure(): void
     {
-        $this
-            ->setDescription('Add a short description for your command')
-            ->addArgument(
-                'arg1',
-                InputArgument::OPTIONAL,
-                'Argument description'
-            )
-            ->addOption(
-                'option1',
-                null,
-                InputOption::VALUE_NONE,
-                'Option description'
-            );
+        $this->setDescription('Scrape an ingress fan site for medal images');
     }
 
     protected function execute(
@@ -207,6 +193,7 @@ class UpdateBadgeDataCommand extends Command
                 'EventBadge',
                 'UniqueBadge_AvenirShardChallenge',
                 'UniqueBadge_Simulacrum',
+                'UniqueBadge_Paragon',
             ],
         ];
 

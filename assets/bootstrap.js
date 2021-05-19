5db221afa15b369102b5b336723d7285ef2db283
Nikolai Plath <der.el.kuku@gmail.com>
2021-05-19 11:33:02 -0500

    Formatting

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,11 +1,12 @@
-import { startStimulusApp } from '@symfony/stimulus-bridge';
-import Clipboard from "stimulus-clipboard"
+import { startStimulusApp } from '@symfony/stimulus-bridge'
+
+import Clipboard from 'stimulus-clipboard'
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
     true,
     /\.(j|t)sx?$/
-));
+))
 
-app.register("clipboard", Clipboard)
+app.register('clipboard', Clipboard)

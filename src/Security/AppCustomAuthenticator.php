4b167992a7506d60885e65e5b49623b723b63b46
Nikolai Plath <der.el.kuku@gmail.com>
2021-06-03 10:10:39 -0500

    Update authenticators

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -2,29 +2,28 @@
 
 namespace App\Security;
 
-use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
+use JetBrains\PhpStorm\ArrayShape;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Csrf\CsrfToken;
-use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
-use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 use UnexpectedValueException;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 
-class AppCustomAuthenticator extends AbstractFormLoginAuthenticator
+class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
 {
     use TargetPathTrait;
 
-    public function __construct(private EntityManagerInterface $entityManager, private UrlGeneratorInterface $urlGenerator, private CsrfTokenManagerInterface $csrfTokenManager, private string $appEnv)
-    {
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator,
+        private string $appEnv
+    ) {
     }
 
     public function supports(Request $request): bool
@@ -33,12 +32,34 @@ class AppCustomAuthenticator extends AbstractFormLoginAuthenticator
             && $request->isMethod('POST');
     }
 
-    public function getCredentials(Request $request)
+    protected function getLoginUrl(Request $request): string
     {
+        return $this->urlGenerator->generate('app_login');
+    }
+
+    public function authenticate(Request $request): PassportInterface
+    {
+        if (false === in_array($this->appEnv, ['dev', 'test'])) {
+            throw new UnexpectedValueException('GTFO!');
+        }
+
+        $credentials = $this->getCredentials($request);
+
+        return new SelfValidatingPassport(
+            new UserBadge($credentials['email'] ?? ''),
+            [new CsrfTokenBadge('login', $credentials['csrf_token'] ?? '')]
+        );
+    }
+
+    #[ArrayShape(['email' => "string", 'csrf_token' => "string"])]
+    public function getCredentials(
+        Request $request
+    ): array {
         $credentials = [
-            'email'      => $request->request->get('email'),
+            'email' => $request->request->get('email'),
             'csrf_token' => $request->request->get('_csrf_token'),
         ];
+
         $request->getSession()->set(
             Security::LAST_USERNAME,
             $credentials['email']
@@ -47,43 +68,14 @@ class AppCustomAuthenticator extends AbstractFormLoginAuthenticator
         return $credentials;
     }
 
-    public function getUser($credentials, UserProviderInterface $userProvider)
-    {
-        $token = new CsrfToken('authenticate', $credentials['csrf_token']);
-        if (!$this->csrfTokenManager->isTokenValid($token)) {
-            throw new InvalidCsrfTokenException();
-        }
-
-        $user = $this->entityManager->getRepository(User::class)->findOneBy(
-            ['email' => $credentials['email']]
-        );
-
-        if (!$user) {
-            throw new CustomUserMessageAuthenticationException(
-                'User could not be found.'
-            );
-        }
-
-        return $user;
-    }
-
-    public function checkCredentials($credentials, UserInterface $user): bool
-    {
-        if (false === in_array($this->appEnv, ['dev', 'test'])) {
-            throw new UnexpectedValueException('GTFO!');
-        }
-
-        return true;
-    }
-
     public function onAuthenticationSuccess(
         Request $request,
         TokenInterface $token,
-        $providerKey
-    ) {
+        $firewallName
+    ): RedirectResponse {
         $targetPath = $this->getTargetPath(
             $request->getSession(),
-            $providerKey
+            $firewallName
         );
 
         if ($targetPath) {
@@ -91,10 +83,5 @@ class AppCustomAuthenticator extends AbstractFormLoginAuthenticator
         }
 
         return new RedirectResponse($this->urlGenerator->generate('default'));
-    }
-
-    protected function getLoginUrl(): string
-    {
-        return $this->urlGenerator->generate('app_login');
     }
 }
--- src/Security/GoogleAuthenticator.php
+++ src/Security/GoogleAuthenticator.php
@@ -4,35 +4,42 @@ namespace App\Security;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
-use App\Service\AvatarHelper;
 use App\Service\TelegramAdminMessageHelper;
 use App\Service\TelegramBotHelper;
 use App\Service\TelegramMessageHelper;
 use Doctrine\ORM\EntityManagerInterface;
-use KnpU\OAuth2ClientBundle\Security\Authenticator\SocialAuthenticator;
-use KnpU\OAuth2ClientBundle\Client\Provider\GoogleClient;
+use KnpU\OAuth2ClientBundle\Client\OAuth2ClientInterface;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Provider\GoogleUser;
-use League\OAuth2\Client\Token\AccessToken;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 use TelegramBot\Api\Exception;
-use TelegramBot\Api\InvalidArgumentException;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 
-class GoogleAuthenticator extends SocialAuthenticator
+class GoogleAuthenticator extends AbstractAuthenticator
 {
     use TargetPathTrait;
+
     private UserRepository $userManager;
     private TelegramMessageHelper $telegramMessageHelper;
 
-    public function __construct(private ClientRegistry $clientRegistry, private EntityManagerInterface $entityManager, private UrlGeneratorInterface $urlGenerator, private TelegramBotHelper $telegramBotHelper, private TelegramAdminMessageHelper $telegramAdminMessageHelper, private AvatarHelper $avatarHelper)
-    {
+    public function __construct(
+        private ClientRegistry $clientRegistry,
+        private EntityManagerInterface $entityManager,
+        private UserRepository $userRepository,
+        private UrlGeneratorInterface $urlGenerator,
+        private TelegramBotHelper $telegramBotHelper,
+        private TelegramAdminMessageHelper $telegramAdminMessageHelper,
+    ) {
     }
 
     public function supports(Request $request): bool
@@ -41,86 +48,14 @@ class GoogleAuthenticator extends SocialAuthenticator
         return $request->attributes->get('_route') === 'connect_google_check';
     }
 
-    /**
-     * @return AccessToken|mixed
-     */
-    public function getCredentials(Request $request)
-    {
-        // this method is only called if supports() returns true
-
-        return $this->fetchAccessToken($this->getGoogleClient());
-    }
-
-    /**
-     * @throws Exception
-     * @throws InvalidArgumentException
-     */
-    public function getUser($credentials, UserProviderInterface $userProvider)
-    {
-        /** @var GoogleUser $googleUser */
-        $googleUser = $this->getGoogleClient()
-            ->fetchUserFromToken($credentials);
-
-        $userRepository = $this->entityManager->getRepository(User::class);
-
-        // Fetch user by google id
-        $user = $userRepository->findOneBy(
-            ['googleId' => $googleUser->getId()]
-        );
-
-        if (!$user) {
-            // Fetch user by email - @todo remove
-            $user = $userRepository->findOneBy(
-                ['email' => $googleUser->getEmail()]
-            );
-            if (!$user) {
-                // Register new user
-                $newUser = true;
-                $user = (new User())
-                    ->setEmail($googleUser->getEmail())
-                    ->setGoogleId($googleUser->getId());
-            } else {
-                $newUser = false;
-                // Update existing users google id - @todo remove
-                $user->setGoogleId($googleUser->getId());
-            }
-
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
-
-            if ($newUser) {
-                $groupId = $this->telegramBotHelper->getGroupId('admin');
-                $this->telegramAdminMessageHelper->sendNewUserMessage($groupId, $user);
-            }
-        }
-
-        if (true){//!$user->getAvatar()) {
-            // Update avatar
-            $user->setAvatar($googleUser->getAvatar());
-            $this->avatarHelper->updateAvatar($user);
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
-        }
-
-        return $user;
-    }
-
-    /**
-     * @return GoogleClient
-     */
-    private function getGoogleClient()
-    {
-        return $this->clientRegistry->getClient('google');
-    }
-
     public function onAuthenticationSuccess(
         Request $request,
         TokenInterface $token,
-        $providerKey
+        $firewallName
     ): RedirectResponse {
         $targetPath = $this->getTargetPath(
             $request->getSession(),
-            $providerKey
+            $firewallName
         );
 
         if ($targetPath) {
@@ -143,16 +78,67 @@ class GoogleAuthenticator extends SocialAuthenticator
     }
 
     /**
-     * Called when authentication is needed, but it's not sent.
-     * This redirects to the 'login'.
+     * @throws IdentityProviderException
      */
-    public function start(
-        Request $request,
-        AuthenticationException $authException = null
-    ): RedirectResponse {
-        return new RedirectResponse(
-            '/connect/',
-            Response::HTTP_TEMPORARY_REDIRECT
+    public function authenticate(Request $request): PassportInterface
+    {
+        $token = $this->getGoogleClient()->getAccessToken();
+
+        /** @var GoogleUser $googleUser */
+        $googleUser = $this->getGoogleClient()
+            ->fetchUserFromToken($token);
+
+        $user = $this->getUser($googleUser);
+
+        return new SelfValidatingPassport(
+            new UserBadge($user->getEmail()),
         );
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function getUser(GoogleUser $googleUser): User
+    {
+        // 1) have they logged in with Google before? Easy!
+        if ($user = $this->userRepository->findOneBy(
+            ['googleId' => $googleUser->getId()]
+        )
+        ) {
+            return $user;
+        }
+
+        // @todo remove: Fetch user by email
+        if ($user = $this->userRepository->findOneBy(
+            ['email' => $googleUser->getEmail()]
+        )
+        ) {
+            // @todo remove: Update existing users google id
+            $user->setGoogleId($googleUser->getId());
+        } else {
+            // Register new user
+            $user = (new User())
+                ->setEmail($googleUser->getEmail())
+                ->setGoogleId($googleUser->getId());
+
+            try {
+                $groupId = $this->telegramBotHelper->getGroupId('admin');
+                $this->telegramAdminMessageHelper->sendNewUserMessage(
+                    $groupId,
+                    $user
+                );
+            } catch (\Exception) {
+            }
+        }
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $user;
+    }
+
+    private function getGoogleClient(): OAuth2ClientInterface
+    {
+        return $this->clientRegistry->getClient('google');
     }
 }

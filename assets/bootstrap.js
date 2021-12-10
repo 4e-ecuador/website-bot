8888afe91a8072695d66c837027559f5bc2e37e5
Nikolai Plath <der.el.kuku@gmail.com>
2021-12-10 13:24:02 -0500

    Update deps

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,7 +1,6 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge'
 
 import Clipboard from 'stimulus-clipboard'
-
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',

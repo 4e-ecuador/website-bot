135dd0fcb179fcd48fd5e79e7d4a2dfc93bc3bbb
Nikolai Plath <der.el.kuku@gmail.com>
2022-01-30 09:09:48 -0500

    Add rememberme in login form

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -10,6 +10,7 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
@@ -47,7 +48,10 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
 
         return new SelfValidatingPassport(
             new UserBadge($credentials['email'] ?? ''),
-            [new CsrfTokenBadge('login', $credentials['csrf_token'] ?? '')]
+            [
+                new CsrfTokenBadge('login', $credentials['csrf_token'] ?? ''),
+                new RememberMeBadge()
+            ]
         );
     }
 

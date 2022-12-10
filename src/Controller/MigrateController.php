cbe3737e2420facef032ffec17b9912617207bc4
Nikolai Plath <der.el.kuku@gmail.com>
2022-12-10 05:05:37 -0500

    Fix permissions

--- src/Controller/MigrateController.php
+++ src/Controller/MigrateController.php
@@ -4,15 +4,16 @@ namespace App\Controller;
 
 use App\Entity\AgentStat;
 use App\Repository\AgentStatRepository;
-use App\Service\FileUploader;
 use League\Csv\Reader;
 use League\Csv\Writer;
 use SplTempFileObject;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/migrate')]
+#[IsGranted('ROLE_AGENT')]
 class MigrateController extends BaseController
 {
     #[Route('/', name: 'app_migrate')]

dd90b4aad10f09921a63176c718fe597514d7205
Nikolai Plath <der.el.kuku@gmail.com>
2022-12-10 05:10:31 -0500

    Update utils

--- Makefile
+++ Makefile
@@ -7,4 +7,7 @@ tests:
 	symfony console doctrine:migrations:migrate -n
 	symfony console doctrine:fixtures:load -n
 	symfony php bin/phpunit --testdox $@
+	#vendor/bin/phpstan
+	vendor/bin/rector process --dry-run
+	#tools/php-cs-fixer/vendor/bin/php-cs-fixer fix --dry-run
 .PHONY: tests
--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Symfony\Set\SymfonyLevelSetList;
 use Rector\Set\ValueObject\SetList;
@@ -16,11 +17,12 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
 
-        SymfonyLevelSetList::UP_TO_SYMFONY_60,
+        SymfonyLevelSetList::UP_TO_SYMFONY_62,
         SymfonySetList::SYMFONY_CODE_QUALITY,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        SymfonySetList::SYMFONY_STRICT,
         SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+
+        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
 
         SetList::EARLY_RETURN
     ]);

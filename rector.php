121d48375e0af88730d1346c0020545899be45c0
elkuku <der.el.kuku@gmail.com>
2024-02-18 12:44:26 -0500

    Update rector.php

--- rector.php
+++ rector.php
@@ -3,27 +3,32 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Symfony\Set\SymfonyLevelSetList;
-use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonySetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->symfonyContainerXml(
-        __DIR__.'/var/cache/dev/App_KernelDevDebugContainer.xml'
-    );
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        earlyReturn: true
+    )
+    ->withAttributesSets(
+        symfony: true,
+        doctrine: true
+    )
+    ->withPhpSets()
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ])
+    ->withSets([
+        SymfonySetList::SYMFONY_64,
 
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-
-        SymfonyLevelSetList::UP_TO_SYMFONY_62,
         SymfonySetList::SYMFONY_CODE_QUALITY,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
-
-        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
-
-        SetList::EARLY_RETURN
-    ]);
-};
+    ])
+    ->withRootFiles();

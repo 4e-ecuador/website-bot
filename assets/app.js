1897538d0337adbf38fc083a62ed0322c93c70e9
elkuku <der.el.kuku@gmail.com>
2024-12-22 16:51:25 -0500

    Update recipe symfony/stimulus-bundle

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,6 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
+// start the Stimulus application
+import './bootstrap.js';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './styles/app.css';
@@ -11,9 +8,3 @@ import './css/medals_50.css';
 
 import 'open-iconic/font/css/open-iconic-bootstrap.css'
 
-// start the Stimulus application
-import './bootstrap.js';
-
-console.log('Thiaaas log comes from assets/app.js - welcome to AssetMapper! 🎉');
-
-//import { Dropdown } from 'bootstrap'
